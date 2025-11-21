45213ed5f0c6e07ec6acbfd73dda25333c0a3483
onlyyourza <paseoking456@gmail.com>
2025-11-21 17:41:17 +0700

    12.	Modify the program code in question number 11 again to display the audience’s name for each line

--- Cinema14.java
+++ Cinema14.java
@@ -10,9 +10,9 @@ public class Cinema14 {
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
 
-        System.out.println("Audience in the row 3:");
-        for (String i : audience[2]) {
-            System.out.println(i);
+        System.out.println("Audience name each line");
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row: " + (i + 1) + String.join(", ", audience[i]));
         }
     }
 }
