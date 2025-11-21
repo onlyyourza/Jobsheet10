16d9884d926dcb702243bba75ace880557a8cce2
onlyyourza <paseoking456@gmail.com>
2025-11-21 17:36:30 +0700

    change program code to display the audience’s name on the 3rd line using a for loop

--- Cinema14.java
+++ Cinema14.java
@@ -10,9 +10,9 @@ public class Cinema14 {
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
 
-        System.out.println(audience.length);
-        for (String[] rowAudience : audience) {
-            System.out.println("Length of row: " + rowAudience.length);
+        System.out.println("Audience in the row 3:");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.println(audience[2][i]);
         }
     }
 }
