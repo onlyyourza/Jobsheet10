e485b1ad1645d0f12e51f238c711e486ce25adc8
onlyyourza <paseoking456@gmail.com>
2025-11-28 19:30:19 +0700

    6.	In menu 2, if the seat is empty, replace null with ***

--- CinemaWithScanner14.java
+++ CinemaWithScanner14.java
@@ -42,7 +42,7 @@ public class CinemaWithScanner14 {
                     System.out.println("\n=== AUDIENCE LIST (4x2) ===");
                     for (int i = 0; i < 4; i++) {
                         for (int j = 0; j < 2; j++) {
-                            System.out.print((audience[i][j] == null ? "-" : audience[i][j]) + "\t");
+                            System.out.print((audience[i][j] == null ? "***" : audience[i][j]) + "\t");
                         }
                         System.out.println();
                     }
