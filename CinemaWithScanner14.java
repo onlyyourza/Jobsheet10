c5220214dbf66bdbd45b7f87433a0c6872261c8a
onlyyourza <paseoking456@gmail.com>
2025-11-28 19:13:39 +0700

    2.	Modify the program code to provide the following menu options: -	Menu 1: Input audience data -	Menu 2: Show audience list -	Menu 3: Exit

--- CinemaWithScanner14.java
+++ CinemaWithScanner14.java
@@ -2,29 +2,49 @@ import java.util.Scanner;
 public class CinemaWithScanner14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int row, column;
-        String name,next;
         String[][] audience = new String[4][2];
-
-        while (true) {
-            System.out.print("Enter a name: ");
-            name = sc.nextLine();
-            System.out.print("Enter row number: ");
-            row = sc.nextInt();
-            System.out.println("Enter column number: ");
-            column = sc.nextInt();
+        int choice;
+        do {
+            System.out.println("\n=== CINEMA MENU ===");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+            System.out.print("Choose menu: ");
+            choice = sc.nextInt();
             sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    String name = sc.nextLine();
 
-            audience[row - 1][column -1] = name;
-            System.out.print("Are there any other audiences to be added? (y/n): ");
-            next = sc.nextLine();
+                    System.out.print("Enter row number (1-4): ");
+                    int row = sc.nextInt();
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                    System.out.print("Enter column number (1-2): ");
+                    int column = sc.nextInt();
+                    sc.nextLine();
+
+                    audience[row - 1][column - 1] = name;
+                    System.out.println("Audience added successfully!");
+                    break;
+
+                case 2:
+                    System.out.println("\n=== AUDIENCE LIST (4x2) ===");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            System.out.print((audience[i][j] == null ? "-" : audience[i][j]) + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
             }
-        }
-
-
+        } while (choice != 3);
         sc.close();
     }
 }
